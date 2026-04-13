a9877fb0313ed25b2320fc674b292b6cb77cc8ce
bepinyd <bepinyd@gmail.com>
2026-04-13 15:05:35 +0530

    updated app name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bros2',
+      title: 'Phone2Act',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
